0a06a56af19ca845e66a0a565bb03a2d15d67ff7
obinneji <obinnejic@gmail.com>
2022-10-18 15:17:11 -0700

    printf function

--- 2-number.c
+++ 2-number.c
@@ -55,7 +55,7 @@ int print_rot13(va_list ap, params_t *params)
 	int i, index;
 	int count = 0;
 	char arr[] =
-		"NOPQRSTUVWXYZABCDEFGHIJKLM    nop[qrstuvwxyzabcdefghijklm";
+		"NOPQRSTUVWXYZABCDEFGHIJKLM    nopqrstuvwxyzabcdefghijklm";
 	char *a = va_arg(ap, char *);
 	(void)params;
 
