e85258b36e57997c64fb312504e27548510b5d3f
obinneji <obinnejic@gmail.com>
2022-10-18 14:59:24 -0700

    printf function

--- 2-printf.c
+++ 2-printf.c
@@ -15,7 +15,7 @@ int print_hex(va_list ap, params_t *params)
 	if (params->l_modifier)
 		l = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int)
+		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
 
@@ -43,7 +43,7 @@ int print_HEX(va_list ap, params_t *params)
 
 	if (params->l_modifier)
 		l = (unsigned long)va_arg(ap, unsigned long);
-	else if (param->h_modifier)
+	else if (params->h_modifier)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
